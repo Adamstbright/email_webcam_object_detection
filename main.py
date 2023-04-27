7845bcc0a9cfd3638f3d52fd50e29963273a2f74
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-04-27 18:29:34 +0100

    draw rectangle on object, get object image

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import cv2
 import time
-
+from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
+count = 1
+
 while True:
+    status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -16,7 +22,7 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
 
-    thresh_frame = cv2.threshold(delta_frame, 40, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 35, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
 
@@ -26,7 +32,23 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h, = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 225, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 225, 0), 3)
+        if rectangle.any():
+            status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            images_with_object = all_images[index]
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list == 1 and status_list[1] == 0:
+        send_email()
+
+
+    print(status_list)
 
     cv2.imshow("My video", frame)
     key = cv2.waitKey(1)
