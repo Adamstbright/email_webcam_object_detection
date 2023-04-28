7639bc0876f3460b3af1918e842ef8055d698d9a
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-04-28 12:09:48 +0100

    add email function, add folder cleaning function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import time
 from emailing import send_email
@@ -9,6 +10,12 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 
 while True:
     status = 0
@@ -39,14 +46,14 @@ while True:
             count = count + 1
             all_images = glob.glob("images/*.png")
             index = int(len(all_images) / 2)
-            images_with_object = all_images[index]
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
-    if status_list == 1 and status_list[1] == 0:
-        send_email()
-
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email(image_with_object)
+        clean_folder()
 
     print(status_list)
 
